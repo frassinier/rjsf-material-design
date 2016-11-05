6d415360f28f9bcd83e4e51a238131c4df5e2c78
Fabien Rassinier <frassinier@talend.com>
2016-11-05 11:52:41 +0100

    docs: add more samples

--- stories/index.js
+++ stories/index.js
@@ -6,12 +6,6 @@ import {muiTheme} from 'storybook-addon-material-ui';
 
 import Form from './../src/index';
 
-const sampleNames = [
-    'arrays',
-    'simple',
-    'widgets'
-];
-
 const stories = storiesOf('Material Design Form', module);
 
 stories.addDecorator(muiTheme());
@@ -19,12 +13,17 @@ stories.addDecorator(muiTheme());
 const capitalizeFirstLetter =
     (string) => string.charAt(0).toUpperCase() + string.slice(1);
 
-sampleNames.forEach(sampleName => {
-    const sample = require(`./samples/${sampleName}`);
-    stories.add(capitalizeFirstLetter(sampleName), () => (
-        <Form
-            {...sample}
-            onSubmit={action('Submit')}
-        />
-    ));
-});
+const sampleFilenames = require.context('./samples', true, /.js$/);
+
+const regex = /^.\/(.*).js$/;
+sampleFilenames.keys().forEach(
+    (filename) => {
+        const sampleName = filename.match(regex)[1];
+        const sampleContent = sampleFilenames(filename);
+        stories.add(capitalizeFirstLetter(sampleName), () => (
+            <Form
+                {...sampleContent}
+                onSubmit={action('Submit')}
+            />
+        ));
+    });

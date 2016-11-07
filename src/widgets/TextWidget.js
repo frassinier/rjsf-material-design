5035f55697241470058119fe4fc6257a1622f556
Fabien Rassinier <frassinier@talend.com>
2016-11-07 18:53:50 +0100

    style: remove react check

--- src/widgets/TextWidget.js
+++ src/widgets/TextWidget.js
@@ -4,13 +4,4 @@ import BaseInput from './BaseInput';
 
 const TextWidget = (props) => <BaseInput {...props}/>;
 
-if (process.env.NODE_ENV !== 'production') {
-    TextWidget.propTypes = {
-        value: PropTypes.oneOfType([
-            React.PropTypes.string,
-            React.PropTypes.number,
-        ]),
-    };
-}
-
 export default TextWidget;
